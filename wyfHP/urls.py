902e4c9cf05eaddcdfb31355908812dbf00789d9
wyf <wyf@umich.edu>
2014-12-07 09:39:34 -0800

    add url for UEditor

--- wyfHP/urls.py
+++ wyfHP/urls.py
@@ -12,6 +12,8 @@ urlpatterns = patterns('',
     url(r'^portfolio/$', 'portfolio.views.portfolio'),
     url(r'^portfolio/(?P<i>\d)/detail/$', 'portfolio.views.show'),
     url(r'^$', 'basic.views.home'),
+    
+    url(r'^ueditor/',include('DjangoUeditor.urls' )),
 )
 
 if settings.DEBUG:
